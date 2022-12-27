ac6e5f721f17784c5db8cd4aa5d908648c5021ad
Shigal <shiglaws@gmail.com>
2022-12-27 11:14:05 +0530

    dto for product

---
+++ product-service/src/main/java/com/shigal/productservice/dto/ProductRequest.java
@@ -0,0 +1,23 @@
+package com.shigal.productservice.dto;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
