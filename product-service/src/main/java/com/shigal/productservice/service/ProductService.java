c488ecc9b5e5403836ad85a49db4ba554c2399ae
Shigal <shiglaws@gmail.com>
2022-12-27 13:55:59 +0530

    service implemented

--- product-service/src/main/java/com/shigal/productservice/service/ProductService.java
+++ product-service/src/main/java/com/shigal/productservice/service/ProductService.java
@@ -5,8 +5,13 @@ package com.shigal.productservice.service;/*
  */
 
 import com.shigal.productservice.dto.ProductRequest;
+import com.shigal.productservice.dto.ProductResponse;
+
+import java.util.List;
 
 public interface ProductService {
 
     public void createProduct(ProductRequest productRequest);
+
+    List<ProductResponse> getAllProducts();
 }
--- product-service/src/main/java/com/shigal/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/shigal/productservice/service/ProductServiceImpl.java
@@ -5,12 +5,16 @@ package com.shigal.productservice.service;/*
  */
 
 import com.shigal.productservice.dto.ProductRequest;
+import com.shigal.productservice.dto.ProductResponse;
 import com.shigal.productservice.model.Product;
 import com.shigal.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +33,21 @@ public class ProductServiceImpl implements ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved.", product.getId());
+    }
+
+    @Override
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).collect(Collectors.toList());
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
