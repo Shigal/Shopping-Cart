9a240e6fd3b7137f734c555609bdf5fd6787f41b
Shigal <shiglaws@gmail.com>
2022-12-27 11:15:15 +0530

    endpoint to save product created

---
+++ product-service/src/main/java/com/shigal/productservice/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.shigal.productservice.controller;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+import com.shigal.productservice.dto.ProductRequest;
+import com.shigal.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+}
