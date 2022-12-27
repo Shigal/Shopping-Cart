a320ffc999e7b00e1df3eb9dd49163bde8d46535
Shigal <shiglaws@gmail.com>
2022-12-27 11:14:37 +0530

    save product method added

---
+++ product-service/src/main/java/com/shigal/productservice/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.shigal.productservice.service;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+import com.shigal.productservice.dto.ProductRequest;
+
+public interface ProductService {
+
+    public void createProduct(ProductRequest productRequest);
+}
