ef05af6c0b198abed4986f879ee174fac12a180c
Shigal <shiglaws@gmail.com>
2022-12-28 10:31:55 +0530

    load data into repo method created

--- inventory-service/src/main/java/com/shigal/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/shigal/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.shigal.inventoryservice;
 
+import com.shigal.inventoryservice.model.Inventory;
+import com.shigal.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,24 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("vivo_17_black");
+			inventory.setQuantity(120);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("vivo_19_blue");
+			inventory1.setQuantity(80);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("vivo_19_white");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
